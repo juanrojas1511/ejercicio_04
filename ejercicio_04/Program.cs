73078f849640c57c8fa31f27d6110a40a2691ad1
Juan Paris <juanpajares6@gmail.com>
2023-10-04 22:03:45 -0500

    Agregar archivos de proyecto.

---
+++ ejercicio_04/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresa un número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < numero; i++)
+            {
+                for (int j = 0; j < numero; j++)
+                {
+                    Console.Write("**");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+            Console.Write("Ingresa un número: ");
+            int numer0 = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < numer0; i++)
+            {
+                for (int j = 0; j < numer0; j++)
+                {
+                    Console.Write("**");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+            
+            Console.Write("Ingresa un número: ");
+            int numb = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < numb; i++)
+            {
+                for (int j = 0; j < numb; j++)
+                {
+                    Console.Write("**");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
